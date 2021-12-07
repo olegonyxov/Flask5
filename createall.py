55e306aab495730fcc0fa2039ad4e573e08ec2bf
olegonyxov <olegonyxov@gmail.com>
2021-12-07 08:08:40 +0200

    next

--- createall.py
+++ createall.py
@@ -1,2 +1,3 @@
-# from app import db
-# db.create_all()
+from app import db
+from models import Movie,Genre,MovieGenre
+db.create_all()
--- models.py
+++ models.py
@@ -1,21 +1,27 @@
 from app import db
 
 
+movies_genres = db.table('movies_genres',
+                db.Column('genres_pk', db.Integer, db.ForeignKey('genres.pk'), primary_key=True),
+                db.Column('movies_pk', db.Integer, db.ForeignKey('movies.pk'), primary_key=True),
+                )
+
 class Movie(db.Model):
-    __tablename__ = "Movies"
+    __tablename__ = "movies"
     pk = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(80), unique=True, nullable=False)
     release_date = db.Column(db.Date)
+    tags = db.relationship('Genres', secondary=movies_genres,
+                           backref=db.backref('movies', lazy='dynamic'))
 
 
 class Genre(db.Model):
+    __tablename__ = "genres"
     pk = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    tags = db.relationship('movies', secondary=movies_genres,
+                           backref=db.backref('genres', lazy='dynamic'))
 
-
-class MovieGenre(db.model):
-    pk = db.Column(db.Integer, primary_key=True, nullable=False)
-    movie_pk = db.Column(db.Integer, db.ForeignKey("movies.pk"))
-    genre_pk = db.Column(db.Integer, db.ForeignKey("genre.pk"))
-
-
+# db.create_all()
+#
+#
